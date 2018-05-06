fa973dd180e3a31d9512ae32a6cb4accebc77d69
Emil <emilhein1@gmail.com>
2018-05-06 20:10:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,7 @@ const {
     compare
 } = require('./module/optifunc');
 
-let a = input => {
-    let accu = 0
-    for (let i = 0; i < 100000; i++) {
-        accu = accu + i
-    }
-    return accu
+module.exports = {
+    run,
+    compare
 }
-let b = input => {
-    return 32
-};
-let c = input => input + input + '2'
-
-let test = compare(a, a, 3);
-console.log(test);
